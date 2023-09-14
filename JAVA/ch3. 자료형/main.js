4f1f3aa17b7f0253128013afabec18d8c222b179
shareffect <133354472+shareffect@users.noreply.github.com>
2023-09-14 21:43:26 +0900

    Add files via upload

---
+++ JAVA/ch3. 자료형/main.js
@@ -0,0 +1,6 @@
+let hive ='hello';
+console.log(hive);
+
+
+let message1 = null; // 나중에 쓰고 싶은 변수인데, 일단 비워놓자.
+let message2; // undifine. 에러에 가깝죠.
---
+++ JAVA/ch4.연산자/main.js
@@ -0,0 +1,34 @@
+let x = 9;
+let y = 2;
+
+console.log(++x+y);
+console.log(y+ ++x);
+let result = --x + y;
+
+console.log(result);
+
+
+let a = 2;
+let b = 3;
+console.log(a > b)
+
+
+사용은 == 를 많이 하기는 하는데,
+굳이 타입까지 비교할 꺼면 === 를 쓴다.
+
+
+논리연산자
+
+&&
+
+숫자, 글자는 true 임.
+
+true 는 끝까지 확인하니까 끝 값이 출력되고,
+
+null, 0, NaN, ... 와 같은 false 는
+나오는 그 순간 && 은 멈추고, 해당 값을 출력.
+
+
+
+nan   =  not a number
+
