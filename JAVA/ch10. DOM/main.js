f56041c008eb45d8e99987daf56b1e2efbe1126c
shareffect <133354472+shareffect@users.noreply.github.com>
2023-09-26 21:45:48 +0900

    Add files via upload

--- JAVA/ch10. DOM/main.js
+++ JAVA/ch10. DOM/main.js
@@ -1,3 +1,43 @@
 document.getElementById : HTML 요소에서 고유한 id 를 이용하여 요소를 찾는다.
 document.getElementsByClassName 
 document.getElementsByTagName
+
+(질문)
+let title = document.getElementById("title");
+
+/*
+변수 = 개체
+
+여기서는 값이 매칭되는가, 아니면 좌표값이 지정되는가
+*/
+
+
+속성 제어
+
+메서드임 ()
+input.setAttribute()
+
+.style. 로/  요소를 가져온 후, .style. 를 이용해서 직접 수정 할 수 있늠. 
+여러 묶여있는 객체에는 적옹되지 않음. 하나씩에만 적용 가능.
+
+css에 적어 놓고,
+추가 제거 시키면서 사용
+
+hello2.classList.add("hello2");
+hello2.classList.remove("hello2");
+
+
+
+새로운 요소 만들기
+
+createElement
+appandchild
+insertBefore
+remove
+parentNode
+
+
+remove는 제거라서 다시 살렸다가 없앴다가 하기는 어려움.HTML
+
+data-temp  (html 속성)
+data.temp
---
+++ JAVA/ch11.이벤트/main.js
@@ -0,0 +1,12 @@
+form 태그가 양식을 제출할 때 ( submit )
+
+clicl  
+mousedown 
+mouseup ( 이게 클릭이랑 같습니다 )
+mouseover (자식요소까지 반응)
+mouseenter (해당 요소만 반응)
+mouseout : 마우스가 요소 밖으로 나왔을 때 (자식 요소 까지)
+mouseleave : (해당요소만)
+mousemove : 
+
+
